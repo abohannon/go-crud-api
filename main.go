cd8f9c0b8d4853911b2060be22b24024865149ee
adam.bohannon <adam.bohannon@housecallpro.com>
2024-07-07 09:13:58 -0700

    added DELETE and PUT

--- main.go
+++ main.go
@@ -17,6 +17,10 @@ type Article struct {
 	Content 	string `json:"content"`
 }
 
+type Response struct {
+	Message string `json:"message"`
+}
+
 var Articles []Article
 
 func homePage(w http.ResponseWriter, r *http.Request) {
@@ -55,13 +59,68 @@ func createNewArticle(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(article)
 }
 
+func updateArticle(w http.ResponseWriter, r *http.Request) {
+	reqBody, _ := ioutil.ReadAll(r.Body)
+
+	var updatedArticle Article
+	var id string
+
+	err := json.Unmarshal(reqBody, &updatedArticle)
+
+	if err != nil {
+		log.Printf("Error unmarshaling JSON: %v", err)
+	}
+
+	id = updatedArticle.Id
+
+	if id == "" {
+		log.Println("ID not found")
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(Response{Message: "400 Bad Request: 'id' is required." })
+	}
+
+
+	for index, article := range Articles {
+		if article.Id == id {
+			if (updatedArticle.Title != "") {
+				article.Title = updatedArticle.Title
+			}
+
+			if (updatedArticle.Desc != "") {
+				article.Desc = updatedArticle.Desc
+			}
+
+			if (updatedArticle.Content != "") {
+				article.Content = updatedArticle.Content
+			}
+
+			Articles = append(Articles[:index], article)
+			json.NewEncoder(w).Encode(article)
+		}
+	}
+}
+
+func deleteArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	id := vars["id"]
+
+	for index, article := range Articles {
+		if article.Id == id {
+			Articles = append(Articles[:index], Articles[index+1:]...)
+		}
+	}
+}
+
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 
-	myRouter.HandleFunc("/", homePage)
-	myRouter.HandleFunc("/all", returnAllArticles)
+	myRouter.HandleFunc("/", homePage).Methods("GET")
+	myRouter.HandleFunc("/articles", returnAllArticles).Methods("GET")
 	myRouter.HandleFunc("/article", createNewArticle).Methods("POST")
-	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
+	myRouter.HandleFunc("/article", updateArticle).Methods("PUT")
+	myRouter.HandleFunc("/article/{id}", deleteArticle).Methods("DELETE")
+	myRouter.HandleFunc("/article/{id}", returnSingleArticle).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
 
